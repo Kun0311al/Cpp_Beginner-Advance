ed2086e99544b23543f650715d650f564386c5b3
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-21 17:29:30 +0530

    output types

---
+++ input_output.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main(){
+    //Printing data
+    std::cout<<"this is for input and output"<<std::endl;
+
+    //in this time the output is getting from the declaring veriable
+    int age{19};
+    std::cout<< "my age is:"<<age<<std::endl;
+
+    //this use to showcase error message
+    std::cerr<<"Error:something is wrong"<<std::endl;
+
+    //this is use to showcase log message
+    std::clog<<"log message: something happen"<<std::endl;
+
+}
