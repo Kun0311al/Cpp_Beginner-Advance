f4b15134d5227c1c668dbdb6b7262b5331f0d6f0
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-22 17:06:24 +0530

    data types

---
+++ data_types.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+int main(){
+    int A = 2 ; //int use to store integer value in it but complete number only 
+    char B = 'A'; //char is use to store charecters in it 
+    float C = 6.458; // float is use to store the fractional number
+    double D = 4.76374; /*double is use to store the big fractional number
+    and it store 15 no after the decimal points*/
+    bool E = true; // boolean is use to store the bool value for exa true and false
+    auto F = false; //auto is auto define the type of value
+    auto G = 18; 
+    long H = 8.9743847856364637364;// long is use to store the long fractional number
+    std::cout<<"1 : "<<A<<std::endl;
+    std::cout<<"2 : "<<B<<std::endl;
+    std::cout<<"3 : "<<C<<std::endl;
+    std::cout<<"4 : "<<D<<std::endl;
+    std::cout<<"5 : "<<E<<std::endl;
+    std::cout<<"6 : "<<F<<std::endl;
+    std::cout<<"7 : "<<G<<std::endl;
+    std::cout<<"8 : "<<H<<std::endl;
+}
