97f7ff04a4ee4568b71099014966a6c928ec3188
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-22 17:32:25 +0530

    adding and decrement opperator

--- basic_operations.cpp
+++ basic_operations.cpp
@@ -8,9 +8,17 @@ int main(){
     int Result2 = num_1 * num_2;
     int Result3 = num_1 / num_2;
     int Result4 = num_1 % num_2;
+    int Result5 = num_1++;
+    int Result6 = num_2--;
+    int Result7 = --num_1;
+    int Result8 = ++num_2;
     std::cout<<"result: "<<Result<<std::endl;
     std::cout<<"result1: "<<Result1<<std::endl;
     std::cout<<"result2: "<<Result2<<std::endl;
     std::cout<<"result3: "<<Result3<<std::endl;
     std::cout<<"result4: "<<Result4<<std::endl;
+    std::cout<<"result5: "<<Result5<<std::endl;
+    std::cout<<"result6: "<<Result6<<std::endl;
+    std::cout<<"result7: "<<Result7<<std::endl;
+    std::cout<<"result8: "<<Result8<<std::endl;
 }
