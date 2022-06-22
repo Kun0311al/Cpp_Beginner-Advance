fe9d4cacce15728a20d1da8b2c02946087da3d21
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-22 17:43:44 +0530

    compound operator and captions

--- basic_operations.cpp
+++ basic_operations.cpp
@@ -3,15 +3,30 @@ int main(){
     int num_1 = 5;
     int num_2 {2};
     int Result;
+    //normal arithmatic operation
     Result = num_1 + num_2;
     int Result1 = num_1 - num_2;
     int Result2 = num_1 * num_2;
     int Result3 = num_1 / num_2;
     int Result4 = num_1 % num_2;
+    //increment and decrement operator
     int Result5 = num_1++;
     int Result6 = num_2--;
     int Result7 = --num_1;
     int Result8 = ++num_2;
+    //compound operator
+    int value = 35;
+    value += 2;
+    std::cout<<"result +=: "<<value<<std::endl;
+    value -= 2;
+    std::cout<<"result -=: "<<value<<std::endl;
+    value *= 2;
+    std::cout<<"result *=: "<<value<<std::endl;
+    value /= 2;
+    std::cout<<"result /=: "<<value<<std::endl;
+    value %= 2;
+    std::cout<<"result %=: "<<value<<std::endl;
+    //Results
     std::cout<<"result add: "<<Result<<std::endl;
     std::cout<<"result1 sub: "<<Result1<<std::endl;
     std::cout<<"result2 multiplication: "<<Result2<<std::endl;
