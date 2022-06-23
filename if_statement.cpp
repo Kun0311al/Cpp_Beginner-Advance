1eb09c275d5d58dfc2ccbbb3f76ae90b300a0162
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-23 18:52:36 +0530

    using if else

--- if_statement.cpp
+++ if_statement.cpp
@@ -3,8 +3,9 @@ int main(){
 
 	int number1 {55};
     int number2 {60};
-    bool result = (number1 < number2);//result comes in boolean form	
-	std::cout << std::boolalpha << "result : " << result << std::endl; 	
+    bool result = (number1 < number2);//result comes in boolean form
+    //if statement	
+	/*std::cout << std::boolalpha << "result : " << result << std::endl; 	
 	std::cout << std::endl;
 	std::cout << "free standing if statement" << std::endl;   
     //if(result)
@@ -13,6 +14,15 @@ int main(){
     }    
     //if(!result){
 	if(!(result == true)){
+        std::cout << number1 << " is NOT less than " << number2 << std::endl;
+    }*/
+
+    //if else statement
+    std::cout << std::endl;
+	std::cout << "using the else clause : " << std::endl;	
+	if(result == true){
+        std::cout << number1 << " is less than " << number2 << std::endl;
+    }else{
         std::cout << number1 << " is NOT less than " << number2 << std::endl;
     }
 }
