e5033dc500f76f01e4da6b98e93f85265335845f
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-23 18:49:11 +0530

    using if statement

---
+++ if_statement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main(){
+
+	int number1 {55};
+    int number2 {60};
+    bool result = (number1 < number2);//result comes in boolean form	
+	std::cout << std::boolalpha << "result : " << result << std::endl; 	
+	std::cout << std::endl;
+	std::cout << "free standing if statement" << std::endl;   
+    //if(result)
+	if(result == true){
+        std::cout << number1 << " is less than " << number2 << std::endl;
+    }    
+    //if(!result){
+	if(!(result == true)){
+        std::cout << number1 << " is NOT less than " << number2 << std::endl;
+    }
+}
