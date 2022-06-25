23cfbbbabbf887685ba5e7650f2c3db6ecfad766
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-25 11:53:02 +0530

    for loop imp caomments

--- Loops.cpp
+++ Loops.cpp
@@ -15,7 +15,9 @@ int main(){
     */
 
    //for loop: use for the same
-   //insteed of unsigned int we can also use "size_t" in for loop
+   //insteed of unsigned int we can also use "size_t" in for loop size_t takes the no. positive only
+   /*the declared things in the for loops are anly works in the for loop they are not 
+   useful for whole program*/
    for(unsigned int j{0};j<10;j++){
     std::cout<<"I love you"<<std::endl;
    }
