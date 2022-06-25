4bee9658cbd6c7fa2be17925b0e615f739c5833f
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-25 11:47:43 +0530

    for loop

--- Loops.cpp
+++ Loops.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 int main(){
     //to print i love you 10 times
+    /*
     std::cout<<"I love you"<<std::endl;
     std::cout<<"I love you"<<std::endl;
     std::cout<<"I love you"<<std::endl;
@@ -11,4 +12,11 @@ int main(){
     std::cout<<"I love you"<<std::endl;
     std::cout<<"I love you"<<std::endl;
     std::cout<<"I love you"<<std::endl;
+    */
+
+   //for loop: use for the same
+   //insteed of unsigned int we can also use "size_t" in for loop
+   for(unsigned int j{0};j<10;j++){
+    std::cout<<"I love you"<<std::endl;
+   }
 }
