66e8784b20e48320acb412c2234c414e84e24464
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-25 11:37:38 +0530

    print 10 times without loop

---
+++ Loops.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(){
+    //to print i love you 10 times
+    std::cout<<"I love you"<<std::endl;
+    std::cout<<"I love you"<<std::endl;
+    std::cout<<"I love you"<<std::endl;
+    std::cout<<"I love you"<<std::endl;
+    std::cout<<"I love you"<<std::endl;
+    std::cout<<"I love you"<<std::endl;
+    std::cout<<"I love you"<<std::endl;
+    std::cout<<"I love you"<<std::endl;
+    std::cout<<"I love you"<<std::endl;
+    std::cout<<"I love you"<<std::endl;
+}
