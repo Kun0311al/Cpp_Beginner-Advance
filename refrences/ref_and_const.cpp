5a79785a13382514afc44a67f5582ff862b6559f
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-28 19:46:29 +0530

    trying to change the value using const pointer

--- refrences/ref_and_const.cpp
+++ refrences/ref_and_const.cpp
@@ -4,6 +4,8 @@ int main(){
     std::cout<<"ref without const : "<<std::endl;
     int marks{90};
     int& ref_marks{marks};
+    //const int& ref_marks{marks}; if we use this we can get the compile error
+    //bcoz const variable we cant change
 
     std::cout<<"marks : "<<marks<<std::endl;
     std::cout<<"ref_marks : "<<ref_marks<<std::endl;
@@ -13,6 +15,13 @@ int main(){
     std::cout<<"ref after changing value : "<<std::endl;
     std::cout<<"marks : "<<marks<<std::endl;
     std::cout<<"ref_marks : "<<ref_marks<<std::endl;
-    
+
+    //tryint to change the value by using const pointer
+    const int* const p_marks{&marks};
+    std::cout<<"marks : "<<marks<<std::endl;
+    std::cout<<"p_marks : "<<*p_marks<<std::endl;
+
+    //*p_marks++; we also can't change the value of the const pointer
+
     return 0;
 }
