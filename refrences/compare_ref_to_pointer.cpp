2cb5d97a90d8e987d50ddb6cfa5e1dbeb4761f24
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-28 19:04:33 +0530

    trying to ref refrence towards other

--- refrences/compare_ref_to_pointer.cpp
+++ refrences/compare_ref_to_pointer.cpp
@@ -28,5 +28,17 @@ int main(){
     std::cout<<"ref_int_value : "<<ref_int_value<<std::endl;
     std::cout<<"p_int_value : "<<p_int_value<<std::endl;
     std::cout<<"*p_int_value : "<<*p_int_value<<std::endl;
+    //result:we can change the value by using the both the types ref and pointer bt ref is easy
+
+    int some_other_value{40};
+    ref_int_value=some_other_value;
+    std::cout<<std::endl;
+    std::cout<<"making the ref refrence towords the other address: "<<std::endl;
+    std::cout<<"int_value : "<<int_value<<std::endl;
+    std::cout<<"ref_int_value : "<<ref_int_value<<std::endl;
+    std::cout<<"p_int_value : "<<p_int_value<<std::endl;
+    std::cout<<"*p_int_value : "<<*p_int_value<<std::endl;
+    //result:we cant change the ref to refrence towads other directly for that u have to change the initialization
+    
 
 }
