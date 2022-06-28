1c281f02a3a3bcf9bacc18958f8c971190166a64
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-28 18:55:54 +0530

    modifying the value by using the ref

--- refrences/compare_ref_to_pointer.cpp
+++ refrences/compare_ref_to_pointer.cpp
@@ -19,4 +19,14 @@ int main(){
     std::cout<<"ref_int_value : "<<ref_int_value<<std::endl;
     std::cout<<"p_int_value : "<<p_int_value<<std::endl;
     std::cout<<"*p_int_value : "<<*p_int_value<<std::endl;
+
+    //writing through the refrence
+    ref_int_value= 30;
+
+    std::cout<<std::endl;
+    std::cout<<"int_value : "<<int_value<<std::endl;
+    std::cout<<"ref_int_value : "<<ref_int_value<<std::endl;
+    std::cout<<"p_int_value : "<<p_int_value<<std::endl;
+    std::cout<<"*p_int_value : "<<*p_int_value<<std::endl;
+
 }
