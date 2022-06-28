8b56df698817642302b134eef67195fad43c5734
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-28 18:28:54 +0530

    modification in data by using main variable

--- refrences/declaring_and_using_refrences.cpp
+++ refrences/declaring_and_using_refrences.cpp
@@ -16,5 +16,22 @@ int main(){
     std::cout<<"&ref_int_value : "<<&ref_int_value<<std::endl;
     std::cout<<"ref_double_value : "<<ref_double_value<<std::endl;
     std::cout<<"&ref_double_value : "<<&ref_double_value<<std::endl;
+
+    //data modification by veriable name
+    int_value= 10;
+    double_value= 10.3;
+    std::cout<<std::endl;
+    std::cout<<"int_value : "<<int_value<<std::endl;
+    std::cout<<"&int_value : "<<&int_value<<std::endl;
+    std::cout<<"double_value : "<<double_value<<std::endl;
+    std::cout<<"&double_value : "<<&double_value<<std::endl;
+
+    std::cout<<"ref_int_value : "<<ref_int_value<<std::endl;
+    std::cout<<"&ref_int_value : "<<&ref_int_value<<std::endl;
+    std::cout<<"ref_double_value : "<<ref_double_value<<std::endl;
+    std::cout<<"&ref_double_value : "<<&ref_double_value<<std::endl;
+    //result:after changing the data the address of the variable is same throughout
+
+    
     return 0;
 }
