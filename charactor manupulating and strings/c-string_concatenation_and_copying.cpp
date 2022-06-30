196d58bd769494b280925a454128278fa842a2c9
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-30 13:27:11 +0530

    std::strcat is use to combine two array

---
+++ charactor manupulating and strings/c-string_concatenation_and_copying.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>//std::strcat
+int main(){
+    //for concantenation our string
+    std::cout<<"std::strcat :"<<std::endl;
+    char test1[20]{"HEllo "};
+    char test2[10]{"Kunal"};
+    std::strcat(test1,test2);
+    std::cout<<"concatenation array: "<<test1<<std::endl;
+    std::strcat(test1," How are you??");
+    std::cout<<"concatenation array: "<<test1<<std::endl;
+    return 0;
+}
