3de2b885fbaca33d8887bb4a8b0628925ef7943c
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-30 16:33:03 +0530

    change in array size

--- charactor manupulating and strings/c-string_concatenation_and_copying.cpp
+++ charactor manupulating and strings/c-string_concatenation_and_copying.cpp
@@ -3,7 +3,7 @@
 int main(){
     //for concantenation our string it use to combine to array or array with string
     std::cout<<"std::strcat :"<<std::endl;
-    char test1[20]{"HEllo "};
+    char test1[50]{"HEllo "};
     char test2[10]{"Kunal"};
     std::strcat(test1,test2);
     std::cout<<"concatenation array: "<<test1<<std::endl;
