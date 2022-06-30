daabd14d8d6dfe80496b39b9bb7b7eff82d4de56
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-30 11:47:23 +0530

    std::strlen for the length of the string

--- charactor manupulating and strings/c-string_manupulation.cpp
+++ charactor manupulating and strings/c-string_manupulation.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
+#include<cstring>//std::strlen
 int main(){
-    13:10;
+    //real array and decayedinto pointer
+    const char test1[]{"Hello I'm Kunal Parkar!!..Learning CPP."};
+    //arrays decays into pointer when we use const char*
+    const char* test2 {"Hello I'm Kunal Parkar!!..Learning CPP."};
+    std::cout<<" "<<test1<<std::endl;
+    //strlen ignore null charactor *total char
+    std::cout<<"strlen(test1) : "<<std::strlen(test1)<<std::endl;
+    //includes the null charactors *total char +1null
+    std::cout<<"sizeof(test1) : "<<sizeof(test1)<<std::endl;
+    //strlen still works with decayed array
+    std::cout<<"strlen(test2) : "<<std::strlen(test2)<<std::endl;
+    //print size of pointer
+    std::cout<<"sizeof(test2) : "<<sizeof(test2)<<std::endl;
+
+    //for comparing string 
+    const char* test_value1{"Kunal"};
+    const
     return 0;
 }
