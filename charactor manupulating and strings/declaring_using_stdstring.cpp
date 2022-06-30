f79169a072605d35e20d471295bf66edbc39071e
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-06-30 18:18:16 +0530

    initiallization using std::string

---
+++ charactor manupulating and strings/declaring_using_stdstring.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+int main(){
+    std::string full_message;//empty string
+    std::string new_message{"Hello and learn CPP."};//initiallize a string with the string value
+    std::string link_message{new_message};//initallize with other existing string
+    std::string message{"Hello kunal",5};//initiallize a part of string vith value
+    std::string weird_message{2,'K'};//we get symbol at the place on no.
+    std::string weird_message2(4,'K');//initiallize multiple copies of char
+    std::string wish_message{"Good Morning"};//
+    std::string new_sort_message{wish_message,5,7};//initiallize the part of the existing string 5is index,7is char after that
+
+    std::cout<<"full_message: "<<full_message<<std::endl;
+    std::cout<<"new_message: "<<new_message<<std::endl;
+    std::cout<<"link_message: "<<link_message<<std::endl;
+    std::cout<<"message: "<<message<<std::endl;
+    std::cout<<"weird_message: "<<weird_message<<std::endl;
+    std::cout<<"weird_message2: "<<weird_message2<<std::endl;
+    std::cout<<"wish_message: "<<wish_message<<std::endl;
+    std::cout<<"new_sort_message: "<<new_sort_message<<std::endl;
+
+    //changing std::string at runtime
+    new_message= "std::cout<<weird_message:<<weird_message<<std::endl;";
+    std::cout<<"new_message: "<<new_message<<std::endl;
+    return 0;
+}
