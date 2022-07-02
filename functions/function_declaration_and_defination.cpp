e94a27215c314c222785d21eac5b4122a469e1b5
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-02 15:32:49 +0530

    min function declaration and defination

--- functions/function_declaration_and_defination.cpp
+++ functions/function_declaration_and_defination.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
-//function declaration
+//function declaration,prototype
+//signature doesn't include return type
 int max(int a,int b);
+int min(int a,int b);
 
 int main(){
     int a{},b{};
@@ -8,12 +10,21 @@ int main(){
     std::cin>>a>>b;
     int result = max(a,b);
     std::cout<<"max no.: "<<result<<std::endl;
+    result = min(a,b);
+    std::cout<<"min no.: "<<result<<std::endl;
     return 0;
 }
 
 //function defination and intiallization
 int max(int a,int b){
     if(a>b)
+        return a;
+    else
+        return b;
+}
+
+int min(int a,int b){
+    if(a<b)
         return a;
     else
         return b;
