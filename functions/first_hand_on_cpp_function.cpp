742219b10734987f772b424b56c6f09fe5932b42
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-02 14:35:04 +0530

    return type function

--- functions/first_hand_on_cpp_function.cpp
+++ functions/first_hand_on_cpp_function.cpp
@@ -7,7 +7,7 @@ void enter_bar(unsigned int age)//function parameters
         std::cout<<"sorry you are not mature.."<<std::endl;
     }
     
-}
+}//void are not return function
 void max(int first_no,int second_no)
 {
     if (first_no>second_no)
@@ -18,16 +18,26 @@ void max(int first_no,int second_no)
     {
         std::cout<<"the max no is: "<<second_no<<std::endl;
     }
-    
+}
+//int is the return type function
+int min(int first_no,int second_no){
+    if(first_no<second_no)
+        return first_no;
+    else
+        return second_no;
 }
 int main(){
     unsigned int a{};
     std::cout<<"enter your age: ";
     std::cin>>a;
+    //calling enter_bar
     enter_bar(a);//function argument
     int b,c;
     std::cout<<"enter two integer no.: ";
     std::cin>>b>>c;
+    //calling max
     max(b,c);
+    int result = min(b,c);
+    std::cout<<"min : "<<result<<std::endl;
     return 0;
 }
