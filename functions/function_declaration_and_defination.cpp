4c609c1b4e33dd079f84ab938185a88e48f6f8a6
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-02 15:38:26 +0530

    inc_multi function code

--- functions/function_declaration_and_defination.cpp
+++ functions/function_declaration_and_defination.cpp
@@ -3,6 +3,8 @@
 //signature doesn't include return type
 int max(int a,int b);
 int min(int a,int b);
+int inc_multi(int a, int b);
+
 
 int main(){
     int a{},b{};
@@ -12,6 +14,8 @@ int main(){
     std::cout<<"max no.: "<<result<<std::endl;
     result = min(a,b);
     std::cout<<"min no.: "<<result<<std::endl;
+    result = inc_multi(a,b);
+    std::cout<<"inc multi: "<<result<<std::endl;
     return 0;
 }
 
@@ -28,4 +32,11 @@ int min(int a,int b){
         return a;
     else
         return b;
+}
+
+int inc_multi(int a,int b){
+    int result = ((++a)*(++b));
+    std::cout<<"a after incrementing: "<<a<<std::endl;
+    std::cout<<"b after incrementing: "<<b<<std::endl;
+    return result;
 }
