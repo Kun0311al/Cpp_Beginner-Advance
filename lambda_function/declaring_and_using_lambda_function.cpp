79d85a9fe3b9ace9e68c350116be0ab6cf8241bb
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-03 17:11:54 +0530

    call function using name,without name

--- lambda_function/declaring_and_using_lambda_function.cpp
+++ lambda_function/declaring_and_using_lambda_function.cpp
@@ -4,3 +4,18 @@ syntex is lambda function signature:
                                 [capture list](//parameters)->return type{   
                                     //function body   
                                 };*/
+#include<iostream>
+int main(){
+    //calling the function without a name
+    [](){
+        std::cout<<"Hello world!!"<<std::endl;
+    }();
+
+    //function calling by using the name
+    auto func = [](){
+        std::cout<<"Hello world!!,Kunal parkar"<<std::endl;
+    };
+
+    func();
+    return 0;
+}
