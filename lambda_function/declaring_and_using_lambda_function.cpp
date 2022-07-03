717cbe0faf243dfda24266968128e520c03ba2ef
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-03 17:00:55 +0530

    lambda functio syntex

---
+++ lambda_function/declaring_and_using_lambda_function.cpp
@@ -0,0 +1,6 @@
+/*we can also give the name for calling that function and we 
+also call that function without givint the name to it 
+syntex is lambda function signature:
+                                [capture list](//parameters)->return type{   
+                                    //function body   
+                                };*/
