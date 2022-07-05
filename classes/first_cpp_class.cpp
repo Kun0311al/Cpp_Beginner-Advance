d7e9c3416902323c5beb84d3ecd62f860cd9249f
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-05 23:22:38 +0530

    change the member veriable

--- classes/first_cpp_class.cpp
+++ classes/first_cpp_class.cpp
@@ -4,6 +4,10 @@ const double pi{3.1415926535897932384626433832795};
 
 //class declaration
 class cylinder{
+    // private://in this class the function are only accessible to this class only 
+    //     //member class
+    //     double radius{};
+    //     double height{};
     public:
         double radius{};
         double height{};
@@ -17,6 +21,11 @@ int main(){
 
     //we add the element in the class
     cylinder cylinder1;
+    std::cout<<"volume : "<<cylinder1.volume()<<std::endl;
+
+    //change the member veriable
+    cylinder1.radius = 20;
+    cylinder1.height = 5;
     std::cout<<"volume : "<<cylinder1.volume()<<std::endl;
     return 0;
 }
