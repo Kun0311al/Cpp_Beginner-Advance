13df9f425cfef467479d4decdd682cbc9be0902b
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-06 23:00:37 +0530

    functio seperates by using another cpp

--- classes/class across multiple file/class_cylinder.cpp
+++ classes/class across multiple file/class_cylinder.cpp
@@ -4,3 +4,9 @@ cylinder::cylinder(double rad_param,double height_param){
             cyli_radius = rad_param;
             cyli_height = height_param;
         }
+
+//we can also seprate the function in the class like this
+/*
+cylinder::double volume(){
+            return pi*cyli_radius*cyli_radius*cyli_height;
+        }*/
