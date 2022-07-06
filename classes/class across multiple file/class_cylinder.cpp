d12db962dc6864e7d3818a40b281491146043498
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-06 23:04:49 +0530

    syntex to seprate setter and getter also

--- classes/class across multiple file/class_cylinder.cpp
+++ classes/class across multiple file/class_cylinder.cpp
@@ -10,3 +10,21 @@ cylinder::cylinder(double rad_param,double height_param){
 double cylinder::volume(){
             return pi*cyli_radius*cyli_radius*cyli_height;
         }*/
+
+//we can also seprate the setter and getter in the class like this
+/*
+double cylinder::Get_cyli_radius(){
+    return cyli_radius;
+}
+double cylinder::Get_cyli_height(){
+    return cyli_height;
+}
+
+//setter
+//this is use to change the private member value from the outside
+void cylinder::Set_cyli_radius(double rad_param){
+    cyli_radius = rad_param;
+}
+void cylinder::Set_cyli_height(double height_param){
+    cyli_height = height_param;
+}*/
