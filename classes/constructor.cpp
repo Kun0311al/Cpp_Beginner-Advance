c52b027c2e1ca49820dfd1ed4684c7293964b4c7
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-06 19:08:52 +0530

    constructor which takes parameters

--- classes/constructor.cpp
+++ classes/constructor.cpp
@@ -5,9 +5,14 @@ const double pi{3.1415926535897932384626433832795};
 class cylinder{
     public:
         //constructor which is not taking any parameters
-        cylinder(){
+        /*cylinder(){
             cyli_radius = 2.0;
             cyli_height = 2.0;
+        }*/
+        //constructor which is taking parameters
+        cylinder(double rad_param,double height_param){
+            cyli_radius = rad_param;
+            cyli_height = height_param;
         }
         //function
         double volume(){
@@ -20,8 +25,11 @@ class cylinder{
 };
 
 int main(){
-    cylinder cylinder1;
+    cylinder cylinder1(30,4);
     std::cout<<"volume of cylinder1 : "<<cylinder1.volume()<<std::endl;
+
+    cylinder cylinder2(14,3);
+    std::cout<<"volume of cylinder2 : "<<cylinder2.volume()<<std::endl;
 
     return 0;
 }
