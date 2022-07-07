92bd357f0f998f8f32b90ca12991e642690bafae
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-07 17:01:23 +0530

    add pointer veriable

--- classes/size_of_class_object.cpp
+++ classes/size_of_class_object.cpp
@@ -9,11 +9,13 @@ class dog{
     private:
         size_t legs;
         size_t arms;
+        int* total;
 };
 
 int main(){
     dog dog1;
     std::cout<<"size of (size_t) : "<<sizeof(size_t)<<std::endl;
+    std::cout<<"size of (int*) : "<<sizeof(int*)<<std::endl;
     std::cout<<"size of (dog) : "<<sizeof(dog)<<std::endl;//it shows the total size of the object i.e.size_t legs
                                                             //size_t arms and functions are not count in it
     return 0;
