943bf8b8480f9cb635dfb0395d61784754b75968
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-07 08:26:47 +0530

    take radius value of heap member using ptr

--- classes/manage_class_object_through_pointer.cpp
+++ classes/manage_class_object_through_pointer.cpp
@@ -18,6 +18,24 @@ class cylinder{
         double volume(){
             return pi*cyli_radius*cyli_radius*cyli_height;
         }
+
+        //getter
+        //this os use to get the private member to outside the class
+        double Get_cyli_radius(){
+            return cyli_radius;
+        }
+        double Get_cyli_height(){
+            return cyli_height;
+        }
+
+        //setter
+        //this is use to change the private member value from the outside
+        void Set_cyli_radius(double rad_param){
+            cyli_radius = rad_param;
+        }
+        void Set_cyli_height(double height_param){
+            cyli_height = height_param;
+        }
     private:
         //member veriable
         double cyli_radius {1.0};
@@ -40,6 +58,7 @@ int main(){
     cylinder* ptr_cylinder2 = new cylinder(24,7);//pass the constructor parameter in ptr
 
     std::cout<<"colume2 : "<<ptr_cylinder2->volume()<<std::endl;
+    std::cout<<"radius of cylinder2 : "<<ptr_cylinder2->Get_cyli_radius()<<std::endl;
 
     delete ptr_cylinder2;
     return 0;
