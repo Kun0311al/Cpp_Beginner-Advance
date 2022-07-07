eced10f24cc3bda46737c3fb01a5e225ecf927c8
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-07 16:59:00 +0530

    add the function and check the size

--- classes/size_of_class_object.cpp
+++ classes/size_of_class_object.cpp
@@ -3,13 +3,18 @@
 class dog{
     public:
         dog() = default;
+    void print_total(){
+        //function
+    }
     private:
         size_t legs;
+        size_t arms;
 };
 
 int main(){
     dog dog1;
     std::cout<<"size of (size_t) : "<<sizeof(size_t)<<std::endl;
     std::cout<<"size of (dog) : "<<sizeof(dog)<<std::endl;//it shows the total size of the object i.e.size_t legs
+                                                            //size_t arms and functions are not count in it
     return 0;
 }
