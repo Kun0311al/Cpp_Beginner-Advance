8f02e5546f884efd604ebaf29b4591d6ef46ebf8
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-07 17:06:22 +0530

    string size

--- classes/size_of_class_object.cpp
+++ classes/size_of_class_object.cpp
@@ -16,7 +16,9 @@ int main(){
     dog dog1;
     std::cout<<"size of (size_t) : "<<sizeof(size_t)<<std::endl;
     std::cout<<"size of (int*) : "<<sizeof(int*)<<std::endl;
-    std::cout<<"size of (dog) : "<<sizeof(dog)<<std::endl;//it shows the total size of the object i.e.size_t legs
+    std::cout<<"size of (dog) : "<<sizeof(dog1)<<std::endl;//it shows the total size of the object i.e.size_t legs
                                                             //size_t arms and functions are not count in it
+    std::string sent = "I am the king";
+    std::cout<<"sizeof(string name) : "<<sizeof(sent)<<std::endl;
     return 0;
 }
