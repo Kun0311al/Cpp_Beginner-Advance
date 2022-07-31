1b2ffcf5d53455175a449537f8b4915576d3ccd6
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-31 17:58:11 +0530

    function overloading

--- function_template/template_function_overloading.cpp
+++ function_template/template_function_overloading.cpp
@@ -16,9 +16,21 @@ void over<T>::display(){
     cout<<data<<endl;
 }
 
+template<class T>
+void func(T a){
+    cout<<"call this func(1)"<<endl;
+}
+
+void func(int a){
+    cout<<"call this func(int)"<<endl;
+}
+
 int main(){
     over Kunal(3010);
     Kunal.display();
+
+    func(2);//this function gives the high priority to the same data type function if we have any
+    func(3.2);
 
     return 0;
 }
