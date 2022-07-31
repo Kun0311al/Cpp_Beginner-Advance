d9d77c724a2b751ed29bd59a8faf8c7ded7a52ea
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-31 17:12:04 +0530

    template default parameters

---
+++ function_template/class_template_default_parameters.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+template<class T1=int, class T2 = float, class T3 = char>
+class kunal{
+    public:
+        T1 a;
+        T2 b;
+        T3 c;
+        kunal(T1 x,T2 y,T3 z){
+            a = x;
+            b = y;
+            c = z;
+        }
+        void display(){
+            cout<<"value of a is "<<a<<endl;
+            cout<<"vlaue of b is "<<b<<endl;
+            cout<<"value of c is "<<c<<endl;
+        }
+};
+int main(){
+    kunal<> K1(3 , 4.3 , 'k');
+    K1.display();
+
+    return 0;
+}
