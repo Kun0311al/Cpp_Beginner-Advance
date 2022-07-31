adecda170b685ee90e56892da90551dc68c80d70
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-07-31 17:36:59 +0530

    swap function using template

--- function_template/template_in_function.cpp
+++ function_template/template_in_function.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
+template<class T>
+void swapp(T &a, T &b){
+    T temp = b;
+    b = a;
+    a = temp;
+}
+
 template<class T1,class T2>
 float add(T1 a , T2 b){
     float sum = (a+b)/2;
@@ -11,6 +18,10 @@ int main(){
     float a;
     a = add(2, 4.5);
     cout<<"the sum of the number is "<<a<<endl;
+
+    int x = 3,y = 2;
+    swapp(x,y);
+    cout<<x<<endl<<y;
 
     return 0;
 }
