d389ffda2d32d8ae3274ef05228a26a951352e75
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-08-07 12:33:45 +0530

    notes for the stl containers

---
+++ STL/containers_in_c++_stl.cpp
@@ -0,0 +1,8 @@
+/*container:-object whoes stores data
+algorithms:-procedure to process data
+itrators:-object which points to an elementof a container*/
+/*three types of containers:-sequence container->store data by linear fashion ex:vector,list,double ended queue
+                             associative containers->direct access ex:map,multi-map,set,multi-set
+                             derive containers->real world modeling ex:stack,queue,priority queue*/
+/*vector: randon access(fast),middle incertion/detetion(slow),insertion at end(fast)
+list: randon access(slow),middle incertion/detetion(fast),insertion at end(fast)*/
