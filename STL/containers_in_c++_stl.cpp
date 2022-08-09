f898d52896bfbc2295b42c18eb3cc78fac5fecd3
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-08-09 10:29:53 +0530

    adding more details of the containers

--- STL/containers_in_c++_stl.cpp
+++ STL/containers_in_c++_stl.cpp
@@ -6,3 +6,5 @@ itrators:-object which points to an elementof a container*/
                              derive containers->real world modeling ex:stack,queue,priority queue*/
 /*vector: randon access(fast),middle incertion/detetion(slow),insertion at end(fast)
 list: randon access(slow),middle incertion/detetion(fast),insertion at end(fast)*/
+/*associative containers: all operations fast except random access*/
+/*derived : all operations depends */
