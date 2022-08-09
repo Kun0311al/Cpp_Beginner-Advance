7972343e0aee24c278596643aa9e94f8ad676f81
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-08-09 11:39:26 +0530

    adding the surten elelment more than 1 time by  using the insert

--- STL/vectors_in_c++_stl.cpp
+++ STL/vectors_in_c++_stl.cpp
@@ -30,6 +30,8 @@ int main(){
     display(vec1);
     vec1.insert(iter+2,10);//iter+2 means add the element in the iter + 2 th position in this case the element will add on 3rd position
     display(vec1);
+    vec1.insert(iter+1,4,310);
+    display(vec1);
 
     return 0;
 }
