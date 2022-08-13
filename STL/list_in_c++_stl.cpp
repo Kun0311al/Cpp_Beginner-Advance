4366c8465b6ec659254f4c5442cb551d88875e5d
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-08-13 15:28:57 +0530

    adding the new list

--- STL/list_in_c++_stl.cpp
+++ STL/list_in_c++_stl.cpp
@@ -8,6 +8,7 @@ void display(list<int> &lst){
     {
         cout<<*iter<<" ";
     }
+    cout<<endl;
 }
 
 int main(){
@@ -22,6 +23,19 @@ int main(){
     list2.push_back(15);
 
     display(list2);
+
+    list<int> list1(4);
+    list<int> :: iterator it;
+    it = list1.begin();
+    *it = 33;
+    it++;
+    *it = 9;
+    it++;
+    *it = 38;
+    it++;
+    *it = 26;
+    it++;
+    display(list1);
 
     return 0;
 }
