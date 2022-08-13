cfdf87d72e2ae623f3bdcf84414cc7fc634116dc
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-08-13 15:36:03 +0530

    adding the different pop sample and the comments

--- STL/list_in_c++_stl.cpp
+++ STL/list_in_c++_stl.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void display(list<int> &lst){
     list<int> :: iterator iter;
-    for (iter = lst.begin();iter!=lst.end();iter++)
+    for (iter = lst.begin();iter!=lst.end()/*end position of the list*/;iter++)
     {
         cout<<*iter<<" ";
     }
@@ -16,19 +16,23 @@ int main(){
     //list<int> list2(7);//empty list of size 7
     list<int> list2;
 
-    list2.push_back(3);
+    list2.push_back(3);//add the element fron back
     list2.push_back(10);
     list2.push_back(6);
     list2.push_back(8);
     list2.push_back(15);
 
     display(list2);
+    list2.pop_back();//delete the element from back
+    display(list2);
+    list2.pop_front();//delete the element from front
+    display(list2);
 
     list<int> list1(4);
-    list<int> :: iterator it;
+    list<int> :: iterator it;//iterator is like a pointer
     it = list1.begin();
-    *it = 33;
-    it++;
+    *it = 33;//giving the value to the empty space
+    it++;//change the pointer
     *it = 9;
     it++;
     *it = 38;
