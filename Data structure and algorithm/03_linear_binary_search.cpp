791f0f362df1ad4ec4b13b41a3a115cd9eb53f0b
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-22 02:12:58 +0530

    binary search in array

--- Data structure and algorithm/03_linear_binary_search.cpp
+++ Data structure and algorithm/03_linear_binary_search.cpp
@@ -12,11 +12,32 @@ void lin_search(int arr[], int size, int element){
         }
         
     }
+}
+
+int bin_search(int arr[], int size, int element){
+    int low, mid, high;
+    low = 0;
+    high = size-1;
+    // Keep searching until low <= high
+    while(low<=high){
+        mid = (low + high)/2;
+        if(arr[mid] == element){
+            cout<<mid<<endl;
+        }
+        if(arr[mid]<element){
+            low = mid+1;
+        }
+        else{
+            high = mid -1;
+        }
+    } 
+    return -1;
     
 }
 
 int main(){
-    int arr[20] = {1, 2, 3, 4, 5};
-    int size = 5, element = 3;
-    lin_search(arr, size, element); 
+    int arr[20] = {1, 2, 3, 4, 5, 6};
+    int size = 6, element = 3;
+    lin_search(arr, size, element);
+    bin_search(arr, size, element);
 }
