218b92b6d706358ee0e93cc2f4ae7d241e71e190
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-22 01:09:58 +0530

    deletion from array

--- Data structure and algorithm/02_array_operations.cpp
+++ Data structure and algorithm/02_array_operations.cpp
@@ -11,6 +11,7 @@ void display(int arr[], int n){
 }
 
 void insert(int arr[], int size_a, int element, int capacity, int index){
+    //insertion
     if (size_a>=capacity){
         cout<<"array is full. you can not add data.\n";
     }
@@ -21,11 +22,26 @@ void insert(int arr[], int size_a, int element, int capacity, int index){
 
 }
 
+void delete_ele(int arr[], int size, int index){
+    //deletion
+    if (size <= index){
+        cout<<"enter valid index.\n";
+    }
+    for (int i = index; i < size-1; i++)
+    {
+        arr[i] = arr[i+1];
+    }
+    
+}
+
 int main(){
     int arr[10] = {1,2,3,4};
     int size = 4, element = 10, index = 3;
     display(arr, size);
     insert(arr, size, element, 100, index);
     size++;
+    display(arr, size);
+    delete_ele(arr, size, index);
+    size--;
     display(arr, size);
 }
