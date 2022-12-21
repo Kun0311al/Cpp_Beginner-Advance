a275421011fb4543cda8bd421114847c8910d1b2
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-22 00:51:37 +0530

    insertion in array

--- Data structure and algorithm/02_array_operations.cpp
+++ Data structure and algorithm/02_array_operations.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void display(int* arr, int n){
+void display(int arr[], int n){
     //traversing the array
     for (int i = 0; i < n; i++)
     {
@@ -10,7 +10,22 @@ void display(int* arr, int n){
     
 }
 
+void insert(int arr[], int size_a, int element, int capacity, int index){
+    if (size_a>=capacity){
+        cout<<"array is full. you can not add data.\n";
+    }
+    for(int i= size_a-1; index <= i; i--){
+        arr[i+1]=arr[i];
+    }
+    arr[index]= element;
+
+}
+
 int main(){
     int arr[10] = {1,2,3,4};
-    display(arr, 4);
+    int size = 4, element = 10, index = 3;
+    display(arr, size);
+    insert(arr, size, element, 100, index);
+    size++;
+    display(arr, size);
 }
