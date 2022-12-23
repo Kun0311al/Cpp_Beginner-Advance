42327b67203234266d3ef857087ac5446d40cd14
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-24 03:36:06 +0530

    creation of circular linked list

---
+++ Data structure and algorithm/07_circular_linked_list_traversal.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+int main(){
+    struct Node* head = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* forth = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* fifth = (struct Node*) malloc(sizeof(struct Node));
+
+    head ->data = 10;
+    head ->next = second;
+
+    second -> data = 04;
+    second -> next = third;
+
+    third -> data = 03;
+    third -> next = forth;
+
+    forth -> data = 05;
+    forth -> next = fifth;
+
+    fifth -> data = 3;
+    fifth -> next = head;
+
+    traverse_list(head);
+    cout<<endl;
+}
