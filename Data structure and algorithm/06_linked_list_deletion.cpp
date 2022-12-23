a58d98f6c0b65a3fb97842de441a7b35ad10d43a
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-24 02:58:45 +0530

    deletion by given value

--- Data structure and algorithm/06_linked_list_deletion.cpp
+++ Data structure and algorithm/06_linked_list_deletion.cpp
@@ -67,18 +67,19 @@ struct Node* delete_end(struct Node* head){
 }
 
 struct Node* delete_value_node(struct Node* head, int value){
-    struct Node* p = head;
-    struct Node* q = head->next;
-
-    while (q->data!=value)
+    struct Node *p = head;
+    struct Node *q = head->next;
+    while(q->data!=value && q->next!= NULL)
     {
         p = p->next;
         q = q->next;
     }
-    p->next = q->next;
-    free(q);
-    return(head);
     
+    if(q->data == value){
+        p->next = q->next;
+        free(q);
+    }
+    return head;
 }
 
 int main(){
