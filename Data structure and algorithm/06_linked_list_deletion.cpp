fc9e882d93c3329d1df52b7a4f527525934680bc
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-24 02:08:32 +0530

    traversing and deleting the first node of list

---
+++ Data structure and algorithm/06_linked_list_deletion.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+void traverse_list(struct Node* ptr){
+    while(ptr!=NULL){
+        cout<<ptr->data<<endl;
+        ptr = ptr->next;
+    }
+}
+
+struct Node* delete_start(struct Node* head){
+    struct Node* ptr = head;
+    head = head->next;
+    free(ptr);
+    return head;
+}
+
+int main(){
+    struct Node* head = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* forth = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* fifth = (struct Node*) malloc(sizeof(struct Node));
+
+    head ->data = 10;
+    head ->next = second;
+
+    second -> data = 04;
+    second -> next = third;
+
+    third -> data = 03;
+    third -> next = forth;
+
+    forth -> data = 05;
+    forth -> next = fifth;
+
+    fifth -> data = 3;
+    fifth -> next = NULL;
+
+    traverse_list(head);
+    cout<<endl;
+    head = delete_start(head);
+    traverse_list(head);
+    cout<<endl;
+}
