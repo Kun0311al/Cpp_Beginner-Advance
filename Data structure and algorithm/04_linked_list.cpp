7c04db0f941841771b79852c16dcba57337f7cb1
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-23 23:13:22 +0530

    traversal function

--- Data structure and algorithm/04_linked_list.cpp
+++ Data structure and algorithm/04_linked_list.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
 
 struct Node{
@@ -6,7 +7,38 @@ struct Node{
     struct Node* next;
 };
 
+//traversal in linked list
+void travels_list(struct Node* ptr){
+    while (ptr!=NULL)
+    {
+        cout<<ptr -> data<<"\n";
+        ptr = ptr->next;
+    }
+    
+}
+
 int main(){
     struct Node* head;
-    Node *head = new Node (sizeof(struct Node*));
+    struct Node* second;
+    struct Node* third;
+
+    // allocated memory of linked list in heap
+    head = (struct Node*) malloc(sizeof(struct Node));
+    second = (struct Node*) malloc(sizeof(struct Node));
+    third = (struct Node*) malloc(sizeof(struct Node));
+
+    //link the first node to second
+    head -> data = 3;
+    head -> next = second;
+
+    //link second node to third
+    second -> data = 12;
+    second -> next = third;
+
+    //last node in the list
+    third ->data = 5;
+    third -> next = NULL;
+
+    //call traversal function
+    travels_list(head);
 }
