5c037ffced82dae72f50d24560800a8e9f760e5f
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-27 23:20:04 +0530

    commenting alll code for better understanding

--- Data structure and algorithm/14_queue_using_array.cpp
+++ Data structure and algorithm/14_queue_using_array.cpp
@@ -8,6 +8,7 @@ struct queue{
     int* arr;
 };
 
+//checking weather the queue is full or not
 int isfull(struct queue* ary){
     if (ary->r == ary->size-1)
     {
@@ -20,6 +21,7 @@ int isfull(struct queue* ary){
     
 }
 
+//check weather the queue is empty or not
 int isempty(struct queue* ary){
     if (ary->r == ary->f)
     {
@@ -32,6 +34,7 @@ int isempty(struct queue* ary){
     
 }
 
+//for adding new element in the ary or queue
 void enqueue(struct queue* ary, int val){
     if (!isfull(ary))
     {
@@ -39,22 +42,23 @@ void enqueue(struct queue* ary, int val){
     }
     else
     {
-        ary->r++;
-        ary->arr[ary->r] = val;
+        ary->r++;//increment back pointer
+        ary->arr[ary->r] = val;//set value at back pointer
     }
     
 }
 
+//for removing the first element from the queue
 int dequeue(struct queue* ary){
-    int val;
+    int val;//to store deleted value
     if (!isempty(ary))
     {
         cout<<"list is already empty\n";
     }
     else
     {
-        ary->f++;
-        val = ary->arr[ary->f];
+        ary->f++;//incrementing front pointer
+        val = ary->arr[ary->f];//val = deleted value
         return val;
     }
     return 0;
@@ -66,11 +70,13 @@ int main(){
     q->f = q->r = -1;
     q->arr = (int*)malloc(q->size*sizeof(int));
 
+//adding elements in queue
     enqueue(q, 23);
     enqueue(q, 10);
     enqueue(q, 3);
 
-    int val;
+    int val;//for storing the returned value from dequeue function
+    //removing elements from queue
     val = dequeue(q);
     cout<<val;
     cout<<endl;
