3317fa53fa5ff48dd653fb81996eb443b4337402
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-27 23:15:10 +0530

    dequeue from the array

--- Data structure and algorithm/14_queue_using_array.cpp
+++ Data structure and algorithm/14_queue_using_array.cpp
@@ -9,7 +9,7 @@ struct queue{
 };
 
 int isfull(struct queue* ary){
-    if (ary->r = ary->size-1)
+    if (ary->r == ary->size-1)
     {
         return 0;
     }
@@ -20,8 +20,8 @@ int isfull(struct queue* ary){
     
 }
 
-int is(struct queue* ary){
-    if (ary->r = ary->size-1)
+int isempty(struct queue* ary){
+    if (ary->r == ary->f)
     {
         return 0;
     }
@@ -45,13 +45,42 @@ void enqueue(struct queue* ary, int val){
     
 }
 
+int dequeue(struct queue* ary){
+    int val;
+    if (!isempty(ary))
+    {
+        cout<<"list is already empty\n";
+    }
+    else
+    {
+        ary->f++;
+        val = ary->arr[ary->f];
+        return val;
+    }
+    return 0;
+}
+
 int main(){
-    struct queue* q;
+    struct queue* q = (struct queue*)malloc(sizeof(struct queue));
     q->size = 20;
     q->f = q->r = -1;
     q->arr = (int*)malloc(q->size*sizeof(int));
 
     enqueue(q, 23);
+    enqueue(q, 10);
+    enqueue(q, 3);
 
-    return 0;
+    int val;
+    val = dequeue(q);
+    cout<<val;
+    cout<<endl;
+    val = dequeue(q);
+    cout<<val;
+    cout<<endl;
+    val = dequeue(q);
+    cout<<val;
+    cout<<endl;
+    val = dequeue(q);
+    cout<<val;
+
 }
