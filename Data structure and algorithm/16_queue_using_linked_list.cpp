fc62953d4b1e87ea519afc9c2be6de82fe6a302b
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2022-12-28 02:51:06 +0530

    solving the bugs and commenting the code

--- Data structure and algorithm/16_queue_using_linked_list.cpp
+++ Data structure and algorithm/16_queue_using_linked_list.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
 using namespace std;
 
-
+//globally declaration of front and rear pointer
 struct node* f = NULL;
 struct node* r = NULL;
 
+//structure of node
 struct node{
     int data;
     struct node* next;
 };
 
-int enqueue(int val){
+//enqueue the new node in the linked list
+void enqueue(int val){
     struct node* n_node = (struct node*) malloc(sizeof(struct node));
-    if (n_node == NULL)
+    if (n_node == NULL)//rear condition if there is not space in heap to create a node
     {
         cout<<"queue is full\n";
     }
@@ -20,9 +22,9 @@ int enqueue(int val){
     {   
         n_node->data = val;
         n_node->next = NULL;
-        if (f == NULL)
+        if (f == NULL)//at the time of insertion of first node
         {
-            f = r = n_node;
+            f = r = n_node;//rear and front at first new node
         }
         else
         { 
@@ -33,10 +35,11 @@ int enqueue(int val){
     }
 }
 
+//dequeue the node from queue
 int dequeue(){
-    struct node* ptr = f;
+    struct node* ptr = f;//set pointer at first pointer
     int val=-1;
-    if (f == NULL)
+    if (f == NULL)//check queue is empty or not
     {
         cout<<"queue is empty\n";
     }
@@ -52,13 +55,14 @@ int dequeue(){
 
 int main(){
 
+//enqueue the elements
     enqueue(10);
     enqueue(3);
     enqueue(16);
     enqueue(4);
 
     int val;
-
+//dequeue the elements from the list
     val = dequeue();
     cout<<"dequeue element: "<<val<<endl;
     val = dequeue();
