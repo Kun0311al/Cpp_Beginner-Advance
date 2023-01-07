b30060c1af12465a09775eab17597596ff1b360a
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-07 11:01:11 +0530

    fixxing bug and commenting the code

--- Data structure and algorithm/17_bubble_sort.cpp
+++ Data structure and algorithm/17_bubble_sort.cpp
@@ -1,21 +1,24 @@
 #include<iostream>
 using namespace std;
 
+
+//function to print the array
 void print_arr(int* a, int n){
-    for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n; i++)
     {
         cout<<a[i]<<" ";
     }
     
 }
 
+//function for bubble sort the array
 void bubble_sort(int *a, int n){
-    int sorted = 0;
-    for (int i = 0; i < n-1; i++)
+    int sorted = 0;//check the code is sorted or not
+    for (int i = 0; i < n-1; i++)//number of passes
     {
-        cout<<"pass Number = "<<i+1<<endl;
+        cout<<"pass Number = "<<i+1<<endl;//pass number
         sorted = 1;
-        for (int j = 0; j < n-1-i; j++)
+        for (int j = 0; j < n-1-i; j++)//number of comparisons
         {
             if (a[j]>a[j+1])
             {
@@ -23,7 +26,7 @@ void bubble_sort(int *a, int n){
                 sorted = 0;
             }            
         }
-        if (sorted == 1)
+        if (sorted == 1)//if array is already swapped then break
         {
             break;
         }
@@ -34,7 +37,7 @@ void bubble_sort(int *a, int n){
 int main(){
     int a[] = {10,3,5,16,8};
     int b[] = {1,2,3,4,5};
-    int n = 6;
+    int n = 5;
 
     print_arr(a, n);
     cout<<endl;
