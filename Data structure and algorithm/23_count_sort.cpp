05cb8afd5b46b3c279e10f98cbdcd6f7d5871e6a
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-08 03:08:42 +0530

    add max and count sort function

--- Data structure and algorithm/23_count_sort.cpp
+++ Data structure and algorithm/23_count_sort.cpp
@@ -9,9 +9,53 @@ void print_arr(int* a, int n){
     
 }
 
+int maximum(int* a, int n){
+    int max = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (max < a[i])
+        {
+            max = a[i];
+        }
+        
+    }
+    return max;
+}
+void count_sort(int* a, int n){
+    int max = maximum(a, n);
+    int* count = (int*) malloc((max+1)*sizeof(int));
+    for (int i = 0; i < max +1; i++)
+    {
+        count[i]=0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        count[a[i]] = count[a[i]]+1;
+    }
+    int j = 0;
+    int k = 0;
+    while(j<max+1)
+    {
+        if (count[j]>0)
+        {
+            a[k] = j;
+            count[j]--;
+            k++;
+        }else{
+            j++;
+        }
+        
+    }
+    
+    
+}
+
 int main(){
     int a[] = {3, 1, 9, 7, 1, 2, 4};
     int n = 7;
 
+    print_arr(a, n);
+    cout<<endl;
+    count_sort(a, n);
     print_arr(a, n);
 }
