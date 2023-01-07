edb6ca3ce86c34630a401fb25e575ef3215b01c1
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-07 12:09:17 +0530

    commenting the code for better understanding

--- Data structure and algorithm/18_insertion_sort.cpp
+++ Data structure and algorithm/18_insertion_sort.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+//function for printing the array
 void print_arr(int *a, int n){
     for (int i = 0; i < n; i++)
     {
@@ -8,18 +9,19 @@ void print_arr(int *a, int n){
     }
 }
 
+//insertion sort array
 void insertion_sort(int *a, int n){
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//number passes required for sorting array
     {
-        cout<<"pass number = "<<i<<endl;
-        int key = a[i];
-        int j = i-1;
-        while (j>=0 && a[j]>key)
+        cout<<"pass number = "<<i<<endl;//pass number
+        int key = a[i];//set key as the number which is been sorted
+        int j = i-1;//set the pointer at 1 step before key
+        while (j>=0 && a[j]>key)//check j is not -1
         {
-            a[j+1] = a[j];
-            j--;
+            a[j+1] = a[j];//move j th term one step ahead
+            j--;//decrement j for further comparison
         }
-        a[j+1] = key;
+        a[j+1] = key;//set the key at the next position
         
     }
     
@@ -33,4 +35,5 @@ int main(){
     cout<<endl;
     insertion_sort(a, n);
     print_arr(a, n);
+    cout<<endl;
 }
