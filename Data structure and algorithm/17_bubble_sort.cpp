94f4f8a562b48cf12ef6986eee62528d499c6391
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-07 10:28:05 +0530

    for printing the array

--- Data structure and algorithm/17_bubble_sort.cpp
+++ Data structure and algorithm/17_bubble_sort.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+void print_arr(int* a, int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        cout<<a[i]<<" ";
+    }
     
+}
+
+int main(){
+    int a[] = {10,3,5,16,8};
+    int n = 6;
+
+    print_arr(a, n);
 }
