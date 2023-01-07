e453b65580740788b7f3ef78ee7caaedfb578009
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-07 19:34:01 +0530

    commenting the code for better understanding and making the code easy

--- Data structure and algorithm/20_quick_sort.cpp
+++ Data structure and algorithm/20_quick_sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//print the array 
 void print_arr(int *a, int n)
 {
     for (int i = 0; i < n; i++)
@@ -9,41 +10,43 @@ void print_arr(int *a, int n)
     }
 }
 
-int partition(int a[], int low, int high)
+//do partition of the main array and make the sorting simple
+int partition(int a[], int low, int high)//take array low and high pointer
 {
-    int pivot = a[low];
-    int i = low + 1;
-    int j = high;
+    int pivot = a[low];//set pivot at low
+    int i = low + 1;//set i at low +1
+    int j = high;//set j at high
 
-    do
+    do//run code once and then check the condition
     {
-        while (a[i] <= pivot)
+        while (a[i] <= pivot)//move i till i is less than or equal to pivot
         {
             i++;
         }
-        while (a[j] > pivot)
+        while (a[j] > pivot)//move j till j is greater than pivot
         {
             j--;
         }
-        if (i < j)
+        if (i < j)//if the positition of i is less than j then swap the element at that position
         {
             swap(a[i], a[j]);
         }
-    } while (i < j);
+    } while (i < j);//run do while till i is less than j
     
-    swap(a[low], a[j]);
-    return j;
+    swap(a[low], a[j]);//set j th element at low
+    return j;//return the new value of j
 }
 
-void quick_sort(int a[], int low, int high)
+//function to sort the array
+void quick_sort(int a[], int low, int high)//take array low and high pointer
 {
-    int partition_index;
+    int partition_index;//initialize the partition index
 
-    if (low < high)
+    if (low < high)//if the low pointer is less than high then only do
     {
-        partition_index = partition(a, low, high);
-        quick_sort(a, low, partition_index - 1);
-        quick_sort(a, partition_index + 1, high);
+        partition_index = partition(a, low, high);//set partition index at the output of partition function
+        quick_sort(a, low, partition_index - 1);//new sort which takes array, low and partition index -1 pointer
+        quick_sort(a, partition_index + 1, high);//new sort which takes array, partition index +1 and high pointer
     }
 }
 
@@ -56,4 +59,5 @@ int main()
     cout << endl;
     quick_sort(a, 0, n - 1);
     print_arr(a, n);
+    cout<<endl;
 }
