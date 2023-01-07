6ca50e201f7a4b129fc7cbe960098c24ac7e57fa
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-08 02:07:03 +0530

    Update 22_merge_sort.cpp

--- Data structure and algorithm/22_merge_sort.cpp
+++ Data structure and algorithm/22_merge_sort.cpp
@@ -1,42 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void print_arr(int *a, int n){
+void print_arr(int *a, int n)
+{
     for (int i = 0; i < n; i++)
     {
-        cout<<a[i]<<" ";
+        cout << a[i] << " ";
     }
-    
 }
 
-void merge(int a[], int low, int mid, int high){
+void merge(int a[], int low, int mid, int high)
+{
     int i, j, k, b[20];
     i = low;
-    j = mid+1;
+    j = mid + 1;
     k = low;
 
-    while (i<=mid && j<=high)
+    while (i <= mid && j <= high)
     {
-        if (a[i]<=a[j])
+        if (a[i] <= a[j])
         {
             b[k] = a[i];
             k++;
             i++;
         }
-        else{
+        else
+        {
             b[k] = a[j];
             k++;
             j++;
         }
-        
     }
-    while (i<=mid)
+    while (i <= mid)
     {
         b[k] = a[i];
         k++;
         i++;
     }
-    while (j<=high)
+    while (j <= high)
     {
         b[k] = a[j];
         k++;
@@ -48,24 +49,25 @@ void merge(int a[], int low, int mid, int high){
     }
 }
 
-void merge_sort(int a[], int low, int high){
+void merge_sort(int a[], int low, int high)
+{
     int mid;
-    if (low<high)
+    if (low < high)
     {
-        mid = (low+high)/2;
+        mid = (low + high) / 2;
         merge_sort(a, low, mid);
-        merge_sort(a, mid+1, high);
+        merge_sort(a, mid + 1, high);
         merge(a, low, mid, high);
     }
-    
 }
 
-int main(){
+int main()
+{
     int a[] = {1, 30, 23, 16, 3, 18};
     int n = 6;
 
     print_arr(a, n);
-    cout<<endl;
-    merge_sort(a, 0, n-1);
+    cout << endl;
+    merge_sort(a, 0, n - 1);
     print_arr(a, n);
 }
