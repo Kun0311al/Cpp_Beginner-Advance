5fe02b82e32f52010051ba59f86dbaef3dbe4b22
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-07 13:03:43 +0530

    commenting the code for better understanding

--- Data structure and algorithm/19_selection_sort.cpp
+++ Data structure and algorithm/19_selection_sort.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+//function for printing the array elements
 void print_arr(int* a, int n){
     for (int i = 0; i < n; i++)
     {
@@ -8,17 +9,18 @@ void print_arr(int* a, int n){
     }
 }
 
+//function to perform selection sort algorithm
 void selection_sort(int *a, int n){
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//total number of passes
     {
-        int indexOfMin = i;
-        for (int j = i+1; j < n; j++)
+        int indexOfMin = i;//set the i th element as the min
+        for (int j = i+1; j < n; j++)//number of comparisons
         {
-            if (a[j]< a[indexOfMin])
+            if (a[j]< a[indexOfMin])//check the j th element with indexofmin element
             {
-                indexOfMin = j;
+                indexOfMin = j;//if yes, change the indexOfMin
             }
-            swap(a[i],a[indexOfMin]);
+            swap(a[i],a[indexOfMin]);//swap indexOfMin element with t th element
             
         }
         
@@ -27,8 +29,8 @@ void selection_sort(int *a, int n){
 }
 
 int main(){
-    int a[]= {2,1,5,12,7,3};
-    int n = 6;
+    int a[]= {2,1,5,12,7,3};//unsorted array
+    int n = 6;//size of array
 
     print_arr(a, n);
     cout<<endl;
