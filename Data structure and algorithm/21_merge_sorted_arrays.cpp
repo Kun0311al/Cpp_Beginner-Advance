6ab825e6777fe05c77f1dcf3e2a245eb8e897b4c
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-08 01:50:56 +0530

    Update 21_merge_sorted_arrays.cpp

--- Data structure and algorithm/21_merge_sorted_arrays.cpp
+++ Data structure and algorithm/21_merge_sorted_arrays.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+//for printing the elements from array
 void print_arr(int *c, int n){
     for (int i = 0; i < n; i++)
     {
@@ -9,31 +10,32 @@ void print_arr(int *c, int n){
     
 }
 
+//for merging two sorteed array
 void merge_sort(int a[], int b[], int c[], int m, int n){
     int i, j, k;
-    i = j = k = 0;
-    while (i<m && j<n)
+    i = j = k = 0;//set all pointers at 0
+    while (i<m && j<n)//till pointer reach at their high
     {
         if (a[i]<b[j])
         {
-            c[k] = a[i];
+            c[k] = a[i];//add i 3rd array
             i++;
             k++;
         }
         else
         {
-            c[k] = b[j];
+            c[k] = b[j];//add i 3rd array
             j++;
             k++;
         }        
     }
-    while (i<m)
+    while (i<m)//adding all remaining elements from array 1
     {
         c[k] = a[i];
         i++;
         k++;
     }
-    while (j<n)
+    while (j<n)//adding all remaining elements from array 2
     {
         c[k] = b[j];
         j++;
