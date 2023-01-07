95aefe49e1569925b8a99da7e6600e15d9808e08
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-07 10:56:58 +0530

    making a code adaptive
    so we can save unwanted time in swapped array

--- Data structure and algorithm/17_bubble_sort.cpp
+++ Data structure and algorithm/17_bubble_sort.cpp
@@ -10,23 +10,30 @@ void print_arr(int* a, int n){
 }
 
 void bubble_sort(int *a, int n){
+    int sorted = 0;
     for (int i = 0; i < n-1; i++)
     {
-        for (int j = 0; j < n; j++)
+        cout<<"pass Number = "<<i+1<<endl;
+        sorted = 1;
+        for (int j = 0; j < n-1-i; j++)
         {
             if (a[j]>a[j+1])
             {
                 swap(a[j],a[j+1]);
-            }
-            
+                sorted = 0;
+            }            
+        }
+        if (sorted == 1)
+        {
+            break;
         }
         
-    }
-    
+    }    
 }
 
 int main(){
     int a[] = {10,3,5,16,8};
+    int b[] = {1,2,3,4,5};
     int n = 6;
 
     print_arr(a, n);
@@ -34,4 +41,6 @@ int main(){
     bubble_sort(a,n);
     print_arr(a, n);
     cout<<endl;
+    bubble_sort(b, n);
+    print_arr(b,n);
 }
