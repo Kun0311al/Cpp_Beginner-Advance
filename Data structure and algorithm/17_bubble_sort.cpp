6a9c326572cde7a10121e7208a7878df7d0546fa
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-07 10:32:47 +0530

    bubble sort algorithm

--- Data structure and algorithm/17_bubble_sort.cpp
+++ Data structure and algorithm/17_bubble_sort.cpp
@@ -9,9 +9,29 @@ void print_arr(int* a, int n){
     
 }
 
+void bubble_sort(int *a, int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (a[j]>a[j+1])
+            {
+                swap(a[j],a[j+1]);
+            }
+            
+        }
+        
+    }
+    
+}
+
 int main(){
     int a[] = {10,3,5,16,8};
     int n = 6;
 
     print_arr(a, n);
+    cout<<endl;
+    bubble_sort(a,n);
+    print_arr(a, n);
+    cout<<endl;
 }
