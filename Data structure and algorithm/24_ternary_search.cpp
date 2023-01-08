e227be4c7bc0f9114f4b7b7c07f99d2ef205f7f6
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-08 23:29:03 +0530

    commenting code for better understanding

--- Data structure and algorithm/24_ternary_search.cpp
+++ Data structure and algorithm/24_ternary_search.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+//function to print the array
 void print_arr(int* a, int n){
     for (int i = 0; i < n; i++)
     {
@@ -9,30 +10,31 @@ void print_arr(int* a, int n){
     
 }
 
+//ternary function which takes array, start index, rear index, searching element
 int ternary_search(int a[], int l, int r, int search){
-    if(l<=r)
+    if(l<=r)//if start index is greater than rear index
     {
-        int mid_1 = l +(r-l)/3;
-        int mid_2 = r -(r-l)/3;
+        int mid_1 = l +(r-l)/3;//find first mid
+        int mid_2 = r -(r-l)/3;//find 2nd mid
 
-        if (a[mid_1] == search)
+        if (a[mid_1] == search)//element of first mid is equal to search
         {
-            return mid_1;
+            return mid_1;//return mid_1 value
         }
-        if (a[mid_2] == search)
+        if (a[mid_2] == search)//element of second mid is equal to search
         {
             return mid_2;
         }
-        if (search < a[mid_1])
+        if (search < a[mid_1])//search element is less than element at mid_1
         {
-            return ternary_search(a, l, mid_1 -1, search);
-        }else if (a[mid_2] < search)
+            return ternary_search(a, l, mid_1 -1, search);//again call same function with new values
+        }else if (a[mid_2] < search)//search element is greater than element at mid_2
         {
-            return ternary_search(a, mid_2 +1, r, search);
+            return ternary_search(a, mid_2 +1, r, search);//again call same function with new values
         }
-        else
+        else//search element is greater than mid_1 element and less than mid_2 element
         {
-            return ternary_search(a, mid_1 +1, mid_2 -1, search);
+            return ternary_search(a, mid_1 +1, mid_2 -1, search);//again call same function with new values
         }
     }
     return -1;
