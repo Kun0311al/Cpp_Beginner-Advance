3c9c3df23af0adb60d9db222aa69d9ba09216da8
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-01-09 22:52:50 +0530

    formatting and commenting the code better understanding

--- Data structure and algorithm/26_prime_numbers.cpp
+++ Data structure and algorithm/26_prime_numbers.cpp
@@ -1,36 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void prime_number(int search){
-    if (search < 1)
+// function to check the code is prime or not
+void prime_number(int search)
+{
+    if (search < 1) // when number is less than 1 then show wrong number
     {
-        cout<<"Oops something is wrong!\n";
+        cout << "Oops something is wrong!\n";
     }
-    if (search == 1)
+    if (search == 1) // number is equal to 1 then it is not a prime and nor a composite
     {
-        cout<<"this is not a prime and nor a composite.\n";
+        cout << "this is not a prime and nor a composite.\n";
     }
-    else{
-        for (int i = 2; i < search; i++)
+    else
+    {
+        for (int i = 2; i < search; i++) // for value of divisor from 2 to the number-1
         {
-            if (search%i == 0)
+            if (search % i == 0) // check the modules is equal to 0 or not is yes then not prime
             {
-                cout<<"this is not a prime number.\n";
+                cout << "this is not a prime number.\n";
                 break;
-            }            
+            }
         }
-        cout<<"this is prime number,\n";
+        cout << "this is prime number,\n"; // otherwise print this
     }
-    
-    
 }
 
-int main(){
+int main()
+{
     int search{};
 
-    cout<<"Check the number is prime or not.\n";
-    cout<<"Enter the number: \n";
-    cin>>search;
+    cout << "Check the number is prime or not.\n";
+    cout << "Enter the number: \n";
+    cin >> search;
     prime_number(search);
 
     return 0;
