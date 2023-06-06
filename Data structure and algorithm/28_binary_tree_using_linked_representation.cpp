7ae84e8dcc2b1b6d8c0ccadbf3e964ed33b39df8
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-06-07 00:20:10 +0530

    making tree using linked representation

---
+++ Data structure and algorithm/28_binary_tree_using_linked_representation.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+//creating the new node
+struct Node* create_node(int data){
+    struct Node* new_Node;
+    new_Node = (struct Node*) malloc(sizeof(struct Node));//heap memory allocation
+    new_Node->data = data;//setting data
+    new_Node->left = NULL;
+    new_Node->right = NULL;
+
+    return new_Node;
+}
+
+int main(){
+    // //construction the root node
+    // struct Node *p;
+    // p = (struct Node*) malloc(sizeof(struct Node));
+    // p->left = NULL;
+    // p->right = NULL;
+    // p->data = 6;
+
+    // //constructiong the lesf node
+    // struct Node* p1;
+    // p1 = (struct Node*) malloc(sizeof(struct Node));
+    // p1->left = NULL;
+    // p1->right = NULL;
+    // p1->data = 4;
+
+    // //constructiong the right node
+    // struct Node* p2;
+    // p2 = (struct Node*) malloc(sizeof(struct Node));
+    // p2->left = NULL;
+    // p2->right = NULL;
+    // p2->data = 7;
+
+    // //linking node with each other
+    // p->left = p1;
+    // p->right = p2;
+
+    //for avoiding this steps we can direct call function
+    struct  Node* p = create_node(6);
+    struct  Node* p1 = create_node(4);
+    struct  Node* p2 = create_node(7);
+
+    //linking node with each other
+    p->left = p1;
+    p->right = p2;
+    
+    return 0;
+}
