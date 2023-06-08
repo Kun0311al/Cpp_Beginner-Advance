21c2f35cef7693fa18a600e08f70f8f32d678029
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2023-06-08 23:16:34 +0530

    Update 28_binary_tree_using_linked_representation.cpp

--- Data structure and algorithm/28_binary_tree_using_linked_representation.cpp
+++ Data structure and algorithm/28_binary_tree_using_linked_representation.cpp
@@ -1,70 +1,84 @@
-#include<iostream>
-#include<malloc.h>
+#include <iostream>
+#include <malloc.h>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left;
+    struct Node *right;
 };
 
-//creating the new node
-struct Node* create_node(int data){
-    struct Node* new_Node;
-    new_Node = (struct Node*) malloc(sizeof(struct Node));//heap memory allocation
-    new_Node->data = data;//setting data
+// creating the new node
+struct Node *create_node(int data)
+{
+    struct Node *new_Node;
+    new_Node = (struct Node *)malloc(sizeof(struct Node)); // heap memory allocation
+    new_Node->data = data;                                 // setting data
     new_Node->left = NULL;
     new_Node->right = NULL;
 
     return new_Node;
 }
 
-//this code will print pre order of the tree
-void preorder(struct Node* root){
-    if(root != NULL){
-        cout<<root->data<<endl;
+// this code will print pre order of the tree
+void preorder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->data << endl;
         preorder(root->left);
         preorder(root->right);
     }
 }
 
-//this code will print the postorder tree
-void postorder(struct Node* root){
-    if(root != NULL){
+// this code will print the postorder tree
+void postorder(struct Node *root)
+{
+    if (root != NULL)
+    {
         postorder(root->left);
         postorder(root->right);
-        cout<<root->data<<endl;
+        cout << root->data << endl;
     }
 }
 
-//this code will give the output for inorder tree
-void inorder(struct Node* root){
-    if(root != NULL){
+// this code will give the output for inorder tree
+void inorder(struct Node *root)
+{
+    if (root != NULL)
+    {
         inorder(root->left);
-        cout<<root->data<<endl;
+        cout << root->data << endl;
         inorder(root->right);
     }
 }
 
-//function is use to check the tree is binary search tree or not
-int isBST(struct Node* root){
-    static struct Node* prev = NULL;//it holds the prev new root value 
-    if(root!=NULL){
-        if(!isBST(root->left)){//if the left of root is not bst then return 0 (recurcive for left tree)
+// function is use to check the tree is binary search tree or not
+int isBST(struct Node *root)
+{
+    static struct Node *prev = NULL; // it holds the prev new root value
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        { // if the left of root is not bst then return 0 (recurcive for left tree)
             return 0;
         }
-        if(prev!=NULL && root->data <= prev->data){//check weather the prev is not null and prev is greater than root
+        if (prev != NULL && root->data <= prev->data)
+        { // check weather the prev is not null and prev is greater than root
             return 0;
         }
         prev = root;
         return isBST(root->right);
     }
-    else{
-        return 1;//if there is no node then it is BST by default
+    else
+    {
+        return 1; // if there is no node then it is BST by default
     }
 }
 
-int main(){
+int main()
+{
     // //construction the root node
     // struct Node *p;
     // p = (struct Node*) malloc(sizeof(struct Node));
@@ -90,25 +104,26 @@ int main(){
     // p->left = p1;
     // p->right = p2;
 
-    //for avoiding this steps we can direct call function
-    struct  Node* p = create_node(6);
-    struct  Node* p1 = create_node(4);
-    struct  Node* p2 = create_node(7);
-    struct  Node* p3 = create_node(3);
-    struct  Node* p4 = create_node(5);
+    // for avoiding this steps we can direct call function
+    struct Node *p = create_node(6);
+    struct Node *p1 = create_node(4);
+    struct Node *p2 = create_node(7);
+    struct Node *p3 = create_node(3);
+    struct Node *p4 = create_node(5);
 
-    //linking node with each other
+    // linking node with each other
     p->left = p1;
     p->right = p2;
     p1->left = p3;
     p1->right = p4;
 
     preorder(p);
-    cout<<endl;
+    cout << endl;
     postorder(p);
-    cout<<endl;
+    cout << endl;
     inorder(p);
-    cout<<endl<<isBST(p)<<endl;
-    
+    cout << endl
+         << isBST(p) << endl;
+
     return 0;
 }
